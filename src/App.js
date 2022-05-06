5f04c517ee3ba4d0e17808d4c38b8425fb63ea87
yuriquezada <quezadat.yuri@gmail.com>
2022-05-05 21:25:23 -0500

    Add two principal column

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Products from './components/navigator';
+import Box from '@mui/material/Box';
+// import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <Products/>
+        </Grid>
+        <Grid item xs={4}>
+          Second column
+        </Grid>
+      </Grid>
+    </Box>
+      
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// import Demo from './components/navigator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
