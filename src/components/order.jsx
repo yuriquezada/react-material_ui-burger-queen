83979ae210c06371e7024100df1cabe2f162eefb
yuriquezada <quezadat.yuri@gmail.com>
2022-05-06 06:35:06 -0500

    Add new elements in order component

--- src/components/order.jsx
+++ src/components/order.jsx
@@ -1,44 +1,53 @@
 import React from 'react';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
+// import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 
-// import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+
+import IconButton from '@mui/material/IconButton';
 // import Stack from '@mui/material/Stack';
 // import DeleteIcon from '@mui/icons-material/Delete';
 // import AlarmIcon from '@mui/icons-material/Alarm';
-// import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 
-import { Button, CardActionArea, CardActions } from '@mui/material';
+import { CardActionArea, CardActions } from '@mui/material';
 
 function OrderItem() {
     return (
-        <Card sx={{ maxWidth: 345 }}>
+      <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
-          <CardMedia
-            component="img"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over 6,000
-              species, ranging across all continents except Antarctica
-            </Typography>
-          </CardContent>
+          <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={4}>
+                <CardMedia
+                component="img"
+                height="80"
+                image="/static/images/cards/contemplative-reptile.jpg"
+                alt="green iguana"
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <Typography gutterBottom variant="h5" component="div">
+                  Lizard
+                </Typography>
+              </Grid>
+            </Grid>
+          </Box>
         </CardActionArea>
         <CardActions>
-          {/* <IconButton color="primary" aria-label="add to shopping cart">
-            <AddShoppingCartIcon />
-          </IconButton> */}
-          {/* <IconButton aria-label="delete">
-            <DeleteIcon />
-          </IconButton> */}
+          <IconButton aria-label="delete">
+            <RemoveCircleIcon />
+          </IconButton>
+          <Typography gutterBottom variant="h5" component="div">
+                n
+          </Typography>
+          <IconButton color="primary" aria-label="add to shopping cart">
+            <AddCircleIcon />
+          </IconButton>
         </CardActions>
       </Card>
     )
