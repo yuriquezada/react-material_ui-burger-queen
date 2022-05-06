642eb2a669c59428b37693161d309e4c59b501e4
yuriquezada <quezadat.yuri@gmail.com>
2022-05-05 22:16:27 -0500

    Add order-list in App

--- src/App.js
+++ src/App.js
@@ -1,34 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
-import Products from './components/navigator';
+import Products from './components/food-list';
 import Box from '@mui/material/Box';
 // import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import OrderList from './components/order-list';
 
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={8}>
           <Products/>
         </Grid>
         <Grid item xs={4}>
-          Second column
+          <OrderList/>
         </Grid>
       </Grid>
     </Box>
